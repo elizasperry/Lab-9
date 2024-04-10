6cb7a99a88f3273b38734fb722ebf4eabb8c8a1d
victoria4055 <victoria.villasa@ufl.edu>
2024-04-10 11:01:32 -0400

    Hi

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from eliza_encoder import *
+
+# Victoria Villasana decode 86143370
+def decode(encoded_pass):
+    decoded_password = ""
+    for digit in encoded_pass:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_password += decoded_digit
+    return decoded_password
+
+
 def main():
     password = str(input('Enter your password: '))
     print(encoder(password))
